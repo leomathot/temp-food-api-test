62a6f9189f11dc869a61901d48800ec214a45c45
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-05-26 17:03:24 -0300

    Se agrega el método toLowerCase porque la API no funciona bien con mayúsculas

--- busqueda.js
+++ busqueda.js
@@ -4,7 +4,8 @@ let botonBuscar = document.querySelector("#boton-busqueda")
 botonBuscar.addEventListener("click", buscar)
 
 function buscar() {
-    let texto = document.querySelector("#input-busqueda").value
+    let texto = document.querySelector("#input-busqueda").value.toLowerCase()
+    console.log(texto)
 
     // const apiKey = "1cf496bec1294afb93bb885351ea44a8"
     const apiKey = "e1a1056a0a904d569e1a7cae36d6aab1"
